df873e1e876a4da4a82dc0a1328f4715090e7b45
Saad <saad@Saads-MacBook-Pro.local>
2017-12-07 19:51:14 +0500

    no message

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= '';
+$config['base_url']	= 'ec2-34-208-143-61.us-west-2.compute.amazonaws.com';
 
 /*
 |--------------------------------------------------------------------------
